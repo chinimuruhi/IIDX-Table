cff964a9b5489dddf786056fdbd20ebe2006b2d4
NMinakami <tini_4173@icloud.com>
2025-02-23 21:58:13 +0900

    [Fix] SpredSheetからデータ取得テスト

--- src/main.py
+++ src/main.py
@@ -1,9 +1,24 @@
 import requests
 import urllib.request
 import gzip
+import gspread
+import os
+from dotenv import load_dotenv
 
-with open('./dist/sp12.iidx.app.json', mode='w') as f:
-    r = requests.get('https://sp12.iidx.app/api/v1/sheets')
+# 検証環境の.envを読み込み
+load_dotenv()
+
+with open('./dist/iidx11.io.json', mode='w') as f:
+    # spred_sheet取得
+    gc = gspread.api_key(os.getenv('GOOGLE_SHEET_KEY'))
+    # https://docs.google.com/spreadsheets/d/1e7gdUmBk3zUGSxVGC--8p6w2TIWMLBcLzOcmWoeOx6Y/edit?gid=1585306050#gid=1585306050
+    sh = gc.open_by_key('1e7gdUmBk3zUGSxVGC--8p6w2TIWMLBcLzOcmWoeOx6Y')
+    normal = sh.worksheet('ノーマルゲージ').get_all_values()
+    hard = sh.worksheet('ハードゲージ').get_all_values()
+    f.write(str(normal) + str(hard))
+
+with open('./dist/iidx-sp12.github.io.json', mode='w') as f:
+    r = requests.get('https://iidx-sp12.github.io/songs.json')
     f.write(r.text)
 
 with open('./dist/bpim.msqkn310.workers.dev.json', mode='w') as f:
